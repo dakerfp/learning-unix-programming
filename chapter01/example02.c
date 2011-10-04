d47e459fcb18abfaa9efb5f1e55118dcc47212f4
Daker Fernandes Pinheiro <dakerfp@gmail.com>
2011-10-04 00:21:59 -0300

    Add example02 that reads an system conf value by sysconf
    Signed-off-by: Daker Fernandes Pinheiro <dakerfp@gmail.com>

---
+++ chapter01/example02.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#include "ec.h"
+
+int main(int argc, char **argv)
+{
+    enum errors {SysconfError};
+
+EC_TRY(enum errors)
+
+    int pjcontrol;
+
+    // sysconf arg values defined in /usr/include/bits/confname.h
+    EC_EQ_E(pjcontrol = sysconf(_SC_ARG_MAX), 0, SysconfError);
+
+    printf("_SC_ARG_MAX: %d\n", pjcontrol);
+
+    return 0;
+
+EC_EXCEPT_BGN
+    EC_EXCEPT(SysconfError) {
+        printf("Error on _SC_ARG_MAX sysconf reading.");
+
+        return -1;
+    }
+EC_EXCEPT_END
+}
