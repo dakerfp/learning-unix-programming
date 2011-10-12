2042ae53808bd4676e1d3e0ceacff9705bf600ed
Daker Fernandes Pinheiro <daker.pinheiro@openbossa.org>
2011-10-12 16:01:58 -0300

    Add default verbose handler
    Signed-off-by: Daker Fernandes Pinheiro <daker.pinheiro@openbossa.org>

--- lib/args.h
+++ lib/args.h
@@ -20,6 +20,15 @@ struct args_options_scheme {
 #define args_end_of_handler_list {.option_name = "", .option_argc = 0, .handler = NULL}
 #define args_empty_handler_list { {.option_name = "", .option_argc = 0, .handler = NULL} }
 
+#define args_verbose_handler {.option_name = "v", .option_argc = 0, .handler = args_set_verbose}
+
 bool args_handle_options(struct args_options_scheme *options_scheme, int argc, char **argv);
+
+static bool __args_verbose_mode = false;
+bool args_set_verbose(int argc, char **argv) {
+    __args_verbose_mode = true;
+    return true;
+}
+bool args_verbose_mode(void) { return __args_verbose_mode; }
 
 #endif //ARGS_LIB_H
